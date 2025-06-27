4de66dc10215e9a461165a3c3c749c8b91364f1f
Kanchan Gaikawad <kanchangaikawad@gmail.com>
2025-06-27 20:30:05 +0530

    Task Tracker App- added add,delete, set as complete task functionality, created service for task, and component logic

--- kanchan-workspace/projects/task-tracker-app/src/app/app.component.ts
+++ kanchan-workspace/projects/task-tracker-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TaskFormComponent } from "./components/task-form/task-form.component";
+import { TaskListComponent } from "./components/task-list/task-list.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,  TaskListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- kanchan-workspace/projects/task-tracker-app/src/app/services/task.service.ts
+++ kanchan-workspace/projects/task-tracker-app/src/app/services/task.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../model/task';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TaskService {
+  tasks: Task[] = [];
 
-  constructor() { }
+  constructor() {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      this.tasks = JSON.parse(storedTasks);
+    }
+  }
+
+  getTasks(): Task[] {
+    return this.tasks;
+  }
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+    this.saveToStorage();
+  }
+
+  updateTask(editedTask: Task) {
+    const index = this.tasks.findIndex((task) => task.id == editedTask.id);
+    if (index !== -1) {
+      this.tasks[index] = editedTask;
+      this.saveToStorage();
+    }
+  }
+
+  deleteTask(id: number) {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveToStorage();
+  }
+  saveToStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
+  }
 }
